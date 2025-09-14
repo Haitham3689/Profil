97a1d8526b836b188a0d39ce3735812fe703dfce
Haitham3689 <haitham1782848@gmail.com>
2025-09-14 14:15:39 +0200

    verbesserung contact,jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
       <h2>Contact </h2>
 
       <div className="contact-links">
-        <a href="tel:+491772593961">📞 anrufen</a>
+        <a href="tel:+491772593961">📞 Anrufen</a>
         <a href="mailto:h1782848@gmail.com">✉️ E-Mail</a>
         <a href="https://github.com/Haitham3689" target="_blank" rel="noopener noreferrer">
           <Github size={20} /> GitHub
