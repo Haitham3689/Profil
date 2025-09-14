bf8b5d0607e5366213e37aebc801b8a4244ec1cf
Haitham3689 <haitham1782848@gmail.com>
2025-09-14 13:17:46 +0200

    first commit

---
+++ src/Contact.jsx
@@ -0,0 +1,18 @@
+import "./style/contact.css";
+import { Github } from "lucide-react";
+
+export default function Contact() {
+  return (
+    <section className="contact-section">
+      <h2>Contact </h2>
+
+      <div className="contact-links">
+        <a href="tel:+491772593961">📞 anrufen</a>
+        <a href="mailto:h1782848@gmail.com">✉️ E-Mail</a>
+        <a href="https://github.com/Haitham3689" target="_blank" rel="noopener noreferrer">
+          <Github size={20} /> GitHub
+        </a>
+      </div>
+    </section>
+  );
+}
